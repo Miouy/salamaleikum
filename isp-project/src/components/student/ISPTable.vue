563f82a1428023bcb07f08e785d31d851fa87bf9
Miouy <sarsenzhalgas@gmail.com>
2020-10-24 22:38:03 +0600

    disciplins page

---
+++ isp-project/src/components/student/ISPTable.vue
@@ -0,0 +1,19 @@
+<template>
+    <table id="table">
+        <thead>
+            <tr>
+                <th>Код дисциплины</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'table',
+    
+}
+</script>
