4b1a8567a7d226ec6e3f1a3797fd41a21ac5791d
XANALI <53555116+XANALI@users.noreply.github.com>
2020-10-26 20:40:55 +0600

    disciplines components added

---
+++ isp-project/src/components/disciplines_manager/DisciplineItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "DisciplineItem"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ isp-project/src/components/disciplines_manager/EditDiscipline.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "EditDiscipline"
+    }
+</script>
+
+<style scoped>
+
+</style>
