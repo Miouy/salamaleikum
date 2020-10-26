91b11d26a2893ac5fb32ae1dfdb3d028f9732314
XANALI <53555116+XANALI@users.noreply.github.com>
2020-10-26 20:42:26 +0600

    views added

---
+++ isp-project/src/views/Disciplines.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div>
+            <h1 class="discipline-title">Disciplines</h1>
+            <router-link to="/disciplines/all">All</router-link>
+            <router-link to="/disciplines/add">Add</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Disciplines"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .discipline-title {
+        text-align: center;
+        font-family: monospace;
+    }
+</style>
