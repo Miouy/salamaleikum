dac6002b064cfa894b3b9f49f331a4bd28191f1c
XANALI <53555116+XANALI@users.noreply.github.com>
2020-10-26 20:41:53 +0600

    group components added

---
+++ isp-project/src/components/groups_manager/GroupItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <tr class="group-row">
+        <td>{{group.group_name}}</td>
+        <td>{{group.group_specialty.specialty_name}}</td>
+        <td><button type="button" @click.prevent="goEdit">Edit</button></td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        name: "GroupItem",
+        props: {
+            group: {
+                type: Object,
+                required: true,
+            }
+        },
+        methods: {
+            goEdit(){
+                this.$router.push({name: 'EditGroup', params: {groupId: this.group.group_id}});
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .group-row {
+        &:hover {
+            color: #009879;
+        }
+
+        td {
+            padding: 12px 15px;
+
+            button {
+                color: #009879;
+                background-color: #F2F8FD;
+                border: 1px solid #009879;
+                outline: none;
+                padding: 5px;
+                font-family: monospace;
+                font-size: 18px;
+
+                &:hover {
+                    color: #F2F8FD;
+                    background-color: #009879;
+                }
+
+                &:active {
+                    color: #000000;
+                }
+            }
+        }
+    }
+</style>
