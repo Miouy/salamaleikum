e6aa1c8ae245c123ba500d4490566e09c483b3ce
XANALI <53555116+XANALI@users.noreply.github.com>
2020-10-30 22:09:09 +0600

    Added groups

--- isp-project/src/views/Groups.vue
+++ isp-project/src/views/Groups.vue
@@ -2,7 +2,7 @@
     <div>
         <div>
             <h1 class="groups-title">Groups</h1>
-            <router-link to="/groups/all">All</router-link>
+            <router-link to="/groups/">All</router-link>
             <router-link to="/groups/add">Add</router-link>
         </div>
         <router-view></router-view>
