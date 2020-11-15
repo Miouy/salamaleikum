b63434378be0ee52783b5376ecbe0957183807d9
XANALI <53555116+XANALI@users.noreply.github.com>
2020-11-15 18:23:35 +0600

    advisor model and schema added

---
+++ isp-backend/database/migrations/2020_11_15_115212_create_advisors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvisorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advisors', function (Blueprint $table) {
+            $table->id('advisor_id');
+            $table->string('advisor_name');
+            $table->string('advisor_surname');
+            $table->string('advisor_email');
+            $table->string('advisor_password');
+            $table->string('advisor_phone_num');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advisors');
+    }
+}
