0bc77a64faa281c2d1c20316fb42e32ec7e59f22
Miouy <sarsenzhalgas@gmail.com>
2020-11-15 18:48:13 +0600

    Group model and schema added

---
+++ isp-backend/database/migrations/2020_11_15_122754_create_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id('group_id');
+            $table->string('group_name');
+            $table->foreignId('advisor_id')->references('advisor_id')->on('advisors')->onDelete('cascade');
+            $table->foreignId('course_id')->references('course_id')->on('courses')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
