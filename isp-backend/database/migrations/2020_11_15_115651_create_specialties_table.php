b712fb3d6a442eb942847fe364d3f8b74a34da4c
XANALI <53555116+XANALI@users.noreply.github.com>
2020-11-15 18:24:05 +0600

    specialty model and schema added

---
+++ isp-backend/database/migrations/2020_11_15_115651_create_specialties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpecialtiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('specialties', function (Blueprint $table) {
+            $table->id('specialty_id');
+            $table->string('name');
+            $table->string('code');
+            $table->integer('courses_quantity');
+            $table->foreignId('manager_id')->nullable()->references('manager_id')->on('managers')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('specialties');
+    }
+}
