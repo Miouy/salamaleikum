a2457b268db58528040f92545e57e3eaec7f25bb
XANALI <53555116+XANALI@users.noreply.github.com>
2020-11-28 12:01:16 +0600

    manager layout added

--- isp-project/src/router/router.js
+++ isp-project/src/router/router.js
@@ -25,106 +25,114 @@ import StudentLogin from "../components/auth/StudentLogin";
 import StudentRegister from "../components/auth/StudentRegister";
 import ManagerLogin from "../components/auth/ManagerLogin";
 import AdvisorLogin from "../components/auth/AdvisorLogin";
+import ManagerLayout from "../layouts/ManagerLayout";
 
 const routes = [
-    // Specialties
     {
-        path: '/specialties',
-        name: 'Specialties',
-        component: Specialties,
+        path: '/manager',
+        name: 'Manager',
+        component: ManagerLayout,
         children: [
+            // Specialties
             {
-                path: '',
-                component: SpecialtiesTable,
+                path: 'specialties',
+                name: 'Specialties',
+                component: Specialties,
+                children: [
+                    {
+                        path: '',
+                        component: SpecialtiesTable,
+                    },
+                    {
+                        path: 'add',
+                        component: AddSpecialty,
+                    },
+                    {
+                        path: ':specialtyId/edit',
+                        name: 'EditSpecialty',
+                        component: EditSpecialty,
+                    }
+                ]
             },
+            //Groups
             {
-                path: 'add',
-                component: AddSpecialty,
+                path: 'groups',
+                name: 'Groups',
+                component: Groups,
+                children: [
+                    {
+                        path: '',
+                        component: GroupsTable,
+                    },
+                    {
+                        path: 'add',
+                        component: AddGroup,
+                    },
+                    {
+                        path: ':groupId/edit',
+                        name: 'EditGroup',
+                        component: EditGroup,
+                    }
+                ]
             },
+            //Disciplines
             {
-                path: ':specialtyId/edit',
-                name: 'EditSpecialty',
-                component: EditSpecialty,
-            }
-        ]
-    },
-    //Groups
-    {
-        path: '/groups',
-        name: 'Groups',
-        component: Groups,
-        children: [
-            {
-                path: '',
-                component: GroupsTable,
+                path: 'disciplines',
+                name: 'Disciplines',
+                component: Disciplines,
+                children: [
+                    {
+                        path: '',
+                        component: DisciplinesTable,
+                    },
+                    {
+                        path: 'add',
+                        component: AddDiscipline,
+                    },
+                    {
+                        path: ':disciplineId/edit',
+                        name: 'EditDiscipline',
+                        component: EditDiscipline,
+                    }
+                ]
             },
+            //Advisors
             {
-                path: 'add',
-                component: AddGroup,
+                path: 'advisors',
+                name: 'Advisors',
+                component: Advisors,
+                children: [
+                    {
+                        path: '',
+                        component: AdvisorsTable,
+                    },
+                    {
+                        path: 'add',
+                        component: AddAdvisor,
+                    },
+                    {
+                        path: ':advisorId/edit',
+                        name: 'EditAdvisor',
+                        component: EditAdvisor,
+                    }
+                ]
             },
+            //Students
             {
-                path: ':groupId/edit',
-                name: 'EditGroup',
-                component: EditGroup,
-            }
-        ]
-    },
-    //Disciplines
-    {
-        path: '/disciplines',
-        name: 'Disciplines',
-        component: Disciplines,
-        children: [
-            {
-                path: '',
-                component: DisciplinesTable,
-            },
-            {
-                path: 'add',
-                component: AddDiscipline,
-            },
-            {
-                path: ':disciplineId/edit',
-                name: 'EditDiscipline',
-                component: EditDiscipline,
-            }
-        ]
-    },
-    //Advisors
-    {
-        path: '/advisors',
-        name: 'Advisors',
-        component: Advisors,
-        children: [
-            {
-                path: '',
-                component: AdvisorsTable,
-            },
-            {
-                path: 'add',
-                component: AddAdvisor,
-            },
-            {
-                path: ':advisorId/edit',
-                name: 'EditAdvisor',
-                component: EditAdvisor,
-            }
-        ]
-    },
-    //Students
-    {
-        path: '/students',
-        name: 'Students',
-        component: Students,
-        children: [
-            {
-                path: '',
-                component: StudentsTable,
-            },
-            {
-                path: ':studentId/edit',
-                name: 'EditStudent',
-                component: EditStudent,
+                path: 'students',
+                name: 'Students',
+                component: Students,
+                children: [
+                    {
+                        path: '',
+                        component: StudentsTable,
+                    },
+                    {
+                        path: ':studentId/edit',
+                        name: 'EditStudent',
+                        component: EditStudent,
+                    }
+                ]
             }
         ]
     },
