31a852ee52a3484de9716aeb7f90bcf6281e216a
Miouy <sarsenzhalgas@gmail.com>
2020-12-06 15:51:09 +0600

    added new models

---
+++ isp-project/src/layouts/StudentLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="wrapper">
+        <header class="header">
+            <div class="navigation">
+                <i class="fas fa-bars" @click="sidebar"></i>
+                <h3 class="logo">Individual Study Plan</h3>
+            </div>
+            <div class="profile">
+                <h3>Alikhan Ryskhan</h3>
+            </div>
+        </header>
+        <aside v-if="!sidebar_hidden" class="sidebar">
+            <div class="bar-links">
+                <div>
+                    <router-link class="link" to="/enroll">Enroll</router-link>
+                </div>
+            </div>
+        </aside>
+        <div class="main">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
